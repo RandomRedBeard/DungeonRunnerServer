36bf32c4a9a933f8d2d11af380cf1091d4af8ec4
Thomas Jansen <tnj@17cosmicln.net>
2018-09-21 11:07:43 -0400

    removed mutex on attr

--- src/gameres/attr.cpp
+++ src/gameres/attr.cpp
@@ -5,62 +5,44 @@ attr::attr() {
 }
 
 void attr::setStrength(int s) {
-	lock.lock();
 	strength = s;
-	lock.unlock();
 }
 
 int attr::getStrength() {
-	lock.lock();
 	int i = strength;
-	lock.unlock();
 	return i;
 }
 
 void attr::setIntelligence(int i) {
-	lock.lock();
 	intelligence = i;
-	lock.unlock();
 }
 
 int attr::getIntelligence() {
-	lock.lock();
 	int i = intelligence;
-	lock.unlock();
 	return i;
 }
 
 void attr::setDexterity(int d) {
-	lock.lock();
 	dexterity = d;
-	lock.unlock();
 }
 
 int attr::getDexterity() {
-	lock.lock();
 	int i = dexterity;
-	lock.unlock();
 	return i;
 }
 
 void attr::setVitality(int v) {
-	lock.lock();
 	vitality = v;
-	lock.unlock();
 }
 
 int attr::getVitality() {
-	lock.lock();
 	int i = vitality;
-	lock.unlock();
 	return i;
 }
 
 void attr::operator=(attr* a) {
-	lock.lock();
 	strength = a->getStrength();
 	dexterity = a->getDexterity();
 	intelligence = a->getIntelligence();
 	vitality = a->getVitality();
-	lock.unlock();
 }
--- src/gameres/attr.h
+++ src/gameres/attr.h
@@ -1,11 +1,7 @@
 #ifndef ATTR_H_
 #define ATTT_H_
 
-#include <mutex>
-
 class attr {
-	std::mutex lock;
-
 	int strength, intelligence, dexterity, vitality;
 public:
 	attr();
